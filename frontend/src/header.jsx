93818ecaf9dd0f0977e30fde6f5b123f4a00caca
Dibakar paramanik <dibakarparamanik504@gmail.com>
2026-09-20 00:34:18 +0530

    remove node

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -1,11 +1,11 @@
 import "./style/HeaderStyle.css";
+import { GoPerson } from "react-icons/go";
 
 function Header() {
   return (
     <div className="head">
       <div className="MainName">Welcome Solder !</div>
-      <div>
-      </div>
+      <div className="person"><GoPerson/></div>
     </div>
   );
 }
