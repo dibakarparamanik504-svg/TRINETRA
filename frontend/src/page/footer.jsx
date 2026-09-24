e9b7ea113ba939dcafb47d22f056c6dec8ef9943
Dibakar paramanik <dibakarparamanik504@gmail.com>
2026-09-24 13:33:15 +0530

    add backend

--- frontend/src/page/footer.jsx
+++ frontend/src/page/footer.jsx
@@ -5,15 +5,17 @@ import { AiFillSignal } from "react-icons/ai";
 import { FaRobot } from "react-icons/fa";
 import { FaBrain } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function Footer({setName}){
+function Footer(){
+    let [active,setactive]=useState("home");
     return(
         <div className="foot">
-            <div className="homebox box"><Link to="/home"><GoHome/></Link></div>
-            <div className="dashbox"><Link to="/dashboard"><AiFillAppstore/></Link></div>
-            <div className="monebox"><Link to="/monitoring"><FaBrain/></Link></div>
-            <div className="disbox" ><Link to="decision"><FaRobot/></Link></div>
-            <div className="analbox"><Link to="/analysis"><AiFillSignal/></Link></div>
+            <div className={active === "home" ?( "homebox Hactives") : ("homebox")} onClick={()=>{setactive("home")}}><Link to="/home"><GoHome/></Link></div>
+            <div className={active === "dash" ? ("dashbox Dactives") : ("dashbox")} onClick={()=>{setactive("dash")}}><Link to="/dashboard"><AiFillAppstore/></Link></div>
+            <div className={active === "mon" ? ("dashbox Mactives") : ("dashbox")} onClick={()=>{setactive("mon")}}><Link to="/monitoring"><FaBrain/></Link></div>
+            <div className={active === "dis" ? ("dashbox Diactives") : ("dashbox")} onClick={()=>{setactive("dis")}}><Link to="decision"><FaRobot/></Link></div>
+            <div className={active === "ana" ? ("dashbox Aactives") : ("dashbox")} onClick={()=>{setactive("ana")}}><Link to="/analysis"><AiFillSignal/></Link></div>
         </div>
 
     );
